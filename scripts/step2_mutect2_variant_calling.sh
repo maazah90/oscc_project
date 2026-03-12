fe885b31656b752eedb431e81ca6008285afd1b6
maazah <maazah@hotmail.com>
2026-03-12 23:47:19 +0500

    Updated Script 2

--- scripts/step2_mutect2_variant_calling.sh
+++ scripts/step2_mutect2_variant_calling.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ################################
+
 # USER PATHS
+
 ################################
 
 REF=reference/GRCh38.fa
 GNOMAD=resources/af-only-gnomad.vcf.gz
 GNOMAD_COMMON=resources/gnomad_common.vcf.gz
-
-INTERVALS=intervals/exome_targets.bed
 
 RESULTS=results
 
@@ -16,28 +16,49 @@ mkdir -p $RESULTS/mutect2
 mkdir -p logs
 
 ################################
+
 # CREATE PANEL OF NORMALS
+
 ################################
 
 if [ ! -f "$RESULTS/pon.vcf.gz" ]; then
 
 echo "Creating Panel of Normals"
 
-gatk --java-options "-Xmx8G" GenomicsDBImport \
---genomicsdb-workspace-path $RESULTS/pon_db \
-$(for f in $RESULTS/pon/*.vcf.gz; do echo -V $f; done)
+mkdir -p $RESULTS/pon_db
 
-gatk --java-options "-Xmx8G" CreateSomaticPanelOfNormals \
--R $REF \
--V gendb://$RESULTS/pon_db \
+# Collect PoN VCF files
+
+PON_FILES=($RESULTS/pon/*.vcf.gz)
+TOTAL_PON=${#PON_FILES[@]}
+PON_BATCH=10
+
+for ((j=0;j<$TOTAL_PON;j+=PON_BATCH))
+do
+
+echo "Importing PoN batch: ${PON_FILES[@]:j:PON_BATCH}"
+
+gatk --java-options "-Xmx6G" GenomicsDBImport 
+--genomicsdb-workspace-path $RESULTS/pon_db 
+$(for f in "${PON_FILES[@]:j:PON_BATCH}"; do echo -V $f; done)
+
+done
+
+gatk --java-options "-Xmx6G" CreateSomaticPanelOfNormals 
+-R $REF 
+-V gendb://$RESULTS/pon_db 
 -O $RESULTS/pon.vcf.gz
+
+gatk IndexFeatureFile -I $RESULTS/pon.vcf.gz
 
 echo "PoN created"
 
 fi
 
 ################################
+
 # SAMPLE LIST
+
 ################################
 
 SAMPLES=($(cat samples.txt))
@@ -45,7 +66,9 @@ TOTAL=${#SAMPLES[@]}
 BATCH=4
 
 ################################
-# FINAL MUTECT2 LOOP
+
+# MUTECT2 LOOP
+
 ################################
 
 for ((i=0;i<$TOTAL;i+=BATCH))
@@ -61,54 +84,87 @@ do
 FINAL_VCF=$RESULTS/mutect2/${SAMPLE}_filtered.vcf.gz
 
 if [ -f "$FINAL_VCF" ]; then
-    echo "$SAMPLE already complete → skipping"
-    continue
+echo "$SAMPLE already complete → skipping"
+continue
 fi
 
 RECAL_BAM=$RESULTS/recal/${SAMPLE}_recal.bam
 
-################################
-# Mutect2
+UNFILTERED=$RESULTS/mutect2/${SAMPLE}_unfiltered.vcf.gz
+TMP_VCF=$RESULTS/mutect2/${SAMPLE}_unfiltered.vcf.gz.tmp
+
+F1R2=$RESULTS/mutect2/${SAMPLE}_f1r2.tar.gz
+PRIORS=$RESULTS/mutect2/${SAMPLE}_artifact-priors.tar.gz
+
+echo "Running Mutect2 for $SAMPLE"
+
 ################################
 
-gatk --java-options "-Xmx4G" Mutect2 \
--R $REF \
--I $RECAL_BAM \
--L $INTERVALS \
--tumor $SAMPLE \
---panel-of-normals $RESULTS/pon.vcf.gz \
---germline-resource $GNOMAD \
--O $RESULTS/mutect2/${SAMPLE}_unfiltered.vcf.gz \
+# Mutect2
+
+################################
+
+gatk --java-options "-Xmx4G" Mutect2 
+-R $REF 
+-I $RECAL_BAM 
+-tumor $SAMPLE 
+--panel-of-normals $RESULTS/pon.vcf.gz 
+--germline-resource $GNOMAD 
+--f1r2-tar-gz $F1R2 
+-O $TMP_VCF 
 2> logs/${SAMPLE}_mutect2.log
 
-################################
-# contamination
+if [ $? -eq 0 ]; then
+mv $TMP_VCF $UNFILTERED
+else
+echo "Mutect2 failed for $SAMPLE"
+rm -f $TMP_VCF
+continue
+fi
+
 ################################
 
-gatk --java-options "-Xmx4G" GetPileupSummaries \
--I $RECAL_BAM \
--V $GNOMAD_COMMON \
--L $INTERVALS \
+# Learn orientation artifacts
+
+################################
+
+gatk --java-options "-Xmx4G" LearnReadOrientationModel 
+-I $F1R2 
+-O $PRIORS
+
+################################
+
+# Contamination estimation
+
+################################
+
+gatk --java-options "-Xmx4G" GetPileupSummaries 
+-I $RECAL_BAM 
+-V $GNOMAD_COMMON 
 -O $RESULTS/mutect2/${SAMPLE}_pileups.table
 
-gatk --java-options "-Xmx4G" CalculateContamination \
--I $RESULTS/mutect2/${SAMPLE}_pileups.table \
+gatk --java-options "-Xmx4G" CalculateContamination 
+-I $RESULTS/mutect2/${SAMPLE}_pileups.table 
 -O $RESULTS/mutect2/${SAMPLE}_contamination.table
 
 ################################
-# filtering
+
+# Variant filtering
+
 ################################
 
-gatk --java-options "-Xmx4G" FilterMutectCalls \
--R $REF \
--V $RESULTS/mutect2/${SAMPLE}_unfiltered.vcf.gz \
---contamination-table $RESULTS/mutect2/${SAMPLE}_contamination.table \
+gatk --java-options "-Xmx4G" FilterMutectCalls 
+-R $REF 
+-V $UNFILTERED 
+--contamination-table $RESULTS/mutect2/${SAMPLE}_contamination.table 
+--ob-priors $PRIORS 
 -O $FINAL_VCF
 
 echo "$SAMPLE finished"
 
 done
 
+echo ""
 read -p "Batch finished. Press ENTER for next batch..."
 
 done
