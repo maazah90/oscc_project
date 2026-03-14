04632ebf4d5d6cba364884aea401e9c0a9bfa498
maazah <maazah@hotmail.com>
2026-03-14 05:22:04 +0500

    Added BED file and minor changes

--- scripts/step1_generate_PoN_inputs.sh
+++ scripts/step1_generate_PoN_inputs.sh
@@ -1,118 +1,129 @@
 #!/bin/bash
 
 ################################
-
 # USER PATHS
+################################
+# Directory of this script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# Project root is assumed to be parent of scripts folder
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+
+REF=$PROJECT_DIR/reference/GRCh38.fa
+DBSNP=$PROJECT_DIR/known_sites/dbsnp_146.vcf.gz
+MILLS=$PROJECT_DIR/known_sites/Mills_and_1000G_gold_standard.indels.vcf.gz
+BAMDIR=$PROJECT_DIR/aligned
+RESULTS=$PROJECT_DIR/results
+LOGS=$PROJECT_DIR/logs
+INTERVALS=$PROJECT_DIR/intervals/exome_targets.bed
+
+
+# Make sure all result directories exist
+mkdir -p "$RESULTS/recal"
+mkdir -p "$RESULTS/pon"
+mkdir -p "$RESULTS/mutect2"
+mkdir -p "$LOGS"
 
 ################################
-
-REF=reference/GRCh38.fa
-DBSNP=known_sites/dbsnp.vcf.gz
-MILLS=known_sites/Mills_and_1000G_gold_standard.indels.vcf.gz
-
-BAMDIR=aligned
-RESULTS=results
-
-mkdir -p $RESULTS/recal
-mkdir -p $RESULTS/pon
-mkdir -p logs
-
-################################
-
 # SAMPLE LIST
-
 ################################
+SAMPLES=($(cat $PROJECT_DIR/samples.txt))
 
-SAMPLES=($(cat samples.txt))
 TOTAL=${#SAMPLES[@]}
 BATCH=4
 
 ################################
-
 # LOOP OVER BATCHES
-
 ################################
-
 for ((i=0;i<$TOTAL;i+=BATCH))
 do
+    echo "===================================="
+    echo "Processing batch: ${SAMPLES[@]:i:BATCH}"
+    echo "===================================="
 
-echo "===================================="
-echo "Processing batch: ${SAMPLES[@]:i:BATCH}"
-echo "===================================="
+    for SAMPLE in "${SAMPLES[@]:i:BATCH}"
+    do
+        RECAL_BAM=$RESULTS/recal/${SAMPLE}_recal.bam
+        PON_VCF=$RESULTS/pon/${SAMPLE}_pon.vcf.gz
+        TMP_VCF=$RESULTS/pon/${SAMPLE}_pon.vcf.gz.tmp
 
-for SAMPLE in "${SAMPLES[@]:i:BATCH}"
-do
+        # Skip finished samples
+        if [ -f "$PON_VCF" ]; then
+            echo "$SAMPLE already finished → skipping"
+            continue
+        fi
 
-RECAL_BAM=$RESULTS/recal/${SAMPLE}_recal.bam
-PON_VCF=$RESULTS/pon/${SAMPLE}_pon.vcf.gz
-TMP_VCF=$RESULTS/pon/${SAMPLE}_pon.vcf.gz.tmp
+        # Check BAM exists
+        if [ ! -f "$BAMDIR/${SAMPLE}.dedup.bam" ]; then
+            echo "Error: $BAMDIR/${SAMPLE}.dedup.bam not found, skipping $SAMPLE"
+            continue
+        fi
 
-# Skip finished samples
+        echo "Running $SAMPLE"
 
-if [ -f "$PON_VCF" ]; then
-echo "$SAMPLE already finished → skipping"
-continue
-fi
+        ################################
+        # BQSR
+        ################################
+        gatk --java-options "-Xmx4G" BaseRecalibrator \
+            -R $REF \
+            -I $BAMDIR/${SAMPLE}.dedup.bam \
+            --known-sites $DBSNP \
+            --known-sites $MILLS \
+            -L $INTERVALS \
+            -O $RESULTS/recal/${SAMPLE}_recal.table \
+            2> $LOGS/${SAMPLE}_bqsr.log
+            
+        if [ $? -ne 0 ]; then
+   	   echo "BaseRecalibrator failed for $SAMPLE"
+    	   continue
+	fi
 
-echo "Running $SAMPLE"
 
-################################
+        gatk --java-options "-Xmx4G" ApplyBQSR \
+            -R $REF \
+            -I $BAMDIR/${SAMPLE}.dedup.bam \
+            --bqsr-recal-file $RESULTS/recal/${SAMPLE}_recal.table \
+            -O $RECAL_BAM \
+            2>> $LOGS/${SAMPLE}_bqsr.log
+            
+          #Stop if BQSR failed  
+            if [ $? -ne 0 ]; then
+    		echo "BQSR failed for $SAMPLE"
+    		continue
+   	    fi
 
-# BQSR
 
-################################
+        ################################
+        # Index recalibrated BAM
+        ################################
+        samtools index $RECAL_BAM
 
-gatk --java-options "-Xmx4G" BaseRecalibrator 
--R $REF 
--I $BAMDIR/${SAMPLE}.dedup.bam 
---known-sites $DBSNP 
---known-sites $MILLS 
--O $RESULTS/recal/${SAMPLE}_recal.table 
-2> logs/${SAMPLE}_bqsr.log
+        ################################
+        # Mutect2 PoN mode
+        ################################
+	gatk --java-options "-Xmx4G" Mutect2 \
+	     -R $REF \
+	     -I $RECAL_BAM \
+	     -L $INTERVALS \
+	     --max-mnp-distance 0 \
+	     -O $TMP_VCF \
+	     2> $LOGS/${SAMPLE}_pon.log
 
-gatk --java-options "-Xmx4G" ApplyBQSR 
--R $REF 
--I $BAMDIR/${SAMPLE}.dedup.bam 
---bqsr-recal-file $RESULTS/recal/${SAMPLE}_recal.table 
--O $RECAL_BAM 
-2>> logs/${SAMPLE}_bqsr.log
 
-################################
 
-# Index recalibrated BAM
+        # Only rename file if Mutect2 finished successfully
+        if [ $? -eq 0 ]; then
+            mv $TMP_VCF $PON_VCF
+            echo "$SAMPLE finished successfully"
+        else
+            echo "$SAMPLE Mutect2 failed"
+            rm -f $TMP_VCF
+        fi
 
-################################
+    done
 
-samtools index $RECAL_BAM
-
-################################
-
-# Mutect2 PoN mode
-
-################################
-
-gatk --java-options "-Xmx4G" Mutect2 
--R $REF 
--I $RECAL_BAM 
---max-mnp-distance 0 
--O $TMP_VCF 
-2> logs/${SAMPLE}_pon.log
-
-# Only rename file if Mutect2 finished successfully
-
-if [ $? -eq 0 ]; then
-mv $TMP_VCF $PON_VCF
-echo "$SAMPLE finished successfully"
-else
-echo "$SAMPLE Mutect2 failed"
-rm -f $TMP_VCF
-fi
-
-done
-
-echo ""
-echo "Batch completed."
-read -p "Press ENTER to run the next batch..."
+    echo ""
+    echo "Batch completed."
+    read -p "Press ENTER to run the next batch..."
 
 done
 
